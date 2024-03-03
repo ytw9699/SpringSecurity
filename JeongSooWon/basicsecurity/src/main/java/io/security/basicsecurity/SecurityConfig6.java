560bd6e517322b55e916122abff71d30f642d651
TaeWon <ytw9699@naver.com>
2024-03-03 14:52:56 +0900

    feat : 세션 정책

--- JeongSooWon/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig6.java
+++ JeongSooWon/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig6.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfig5 extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig6 extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -18,6 +19,7 @@ public class SecurityConfig5 extends WebSecurityConfigurerAdapter {
                 .formLogin();
         http
                 .sessionManagement()//세션관리 기능이 작동함
-                .sessionFixation().changeSessionId();//changeSessionId 기본값 으로 이미 설정됨, 로그인시 세션아이디 새롭게 변경해줌
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);//기본값
+                //.sessionCreationPolicy(SessionCreationPolicy.STATELESS);//jwt 사용시
     }
 }
