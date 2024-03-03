11d1ddd97699b6eafc8fb1c7a99960d29005b323
TaeWon <ytw9699@naver.com>
2024-03-03 14:30:50 +0900

    feat : 동시 세션 제어

--- JeongSooWon/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig3.java
+++ JeongSooWon/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig3.java
@@ -7,8 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig3 extends WebSecurityConfigurerAdapter {
 
     @Autowired
