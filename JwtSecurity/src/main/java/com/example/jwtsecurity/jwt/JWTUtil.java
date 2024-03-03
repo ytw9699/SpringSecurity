b2b29a5dd7a7fe4943d5c35d425e3f58a183d4ca
TaeWon <ytw9699@naver.com>
2024-03-03 18:18:03 +0900

    feat : JWT 9 : JWT 발급 및 검증 클래스

---
+++ JwtSecurity/src/main/java/com/example/jwtsecurity/jwt/JWTUtil.java
@@ -0,0 +1,48 @@
+package com.example.jwtsecurity.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+                             Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUsername(Jws<Claims> claims) {
+        return claims.getPayload().get("username", String.class);
+    }
+
+    public String getRole(Jws<Claims> claims) {
+        return claims.getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(Jws<Claims> claims) {//토큰 만료확인
+        return claims.getPayload().getExpiration().before(new Date());//현재시간과 비교
+    }
+    public Jws<Claims> getClaims(String token){//verifyWith 토큰이 우리서버에서 생성된건지, 우리가 가지고있는 키랑 맞는지 확인
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {//expiredMs 살아 있을 시간
+        return Jwts.builder()
+                        .claim("username", username)//페이로드
+                        .claim("role", role)//페이로드
+                        .issuedAt(new Date(System.currentTimeMillis()))//현재 발행 시간
+                        .expiration(new Date(System.currentTimeMillis() + expiredMs))//소멸시간
+                        .signWith(secretKey)//시그니처 암호화진행
+                    .compact();//토큰을 컴팩트
+    }
+}
