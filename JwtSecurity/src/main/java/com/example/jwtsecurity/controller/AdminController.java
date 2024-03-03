cf07586742a6ab48cf35880ee173e7d84ece6051
TaeWon <ytw9699@naver.com>
2024-03-03 16:15:45 +0900

    feat : 기본 컨트롤러 생성

---
+++ JwtSecurity/src/main/java/com/example/jwtsecurity/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.example.jwtsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+        return "Admin Controller";
+    }
+}
