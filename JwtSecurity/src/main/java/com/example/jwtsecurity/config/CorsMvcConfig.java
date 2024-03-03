03ee9e6bda22d7155f2d97cfa464431982cd8dbb
TaeWon <ytw9699@naver.com>
2024-03-03 20:00:25 +0900

    feat : JWT 13 : CORS 설정 - 시큐리티 , mvc 2곳 모두

---
+++ JwtSecurity/src/main/java/com/example/jwtsecurity/config/CorsMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.jwtsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {//컨트롤러에서 cores 뚫어줌
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
--- JwtSecurity/src/main/java/com/example/jwtsecurity/config/SecurityConfig.java
+++ JwtSecurity/src/main/java/com/example/jwtsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.jwtsecurity.config;
 import com.example.jwtsecurity.jwt.JWTFilter;
 import com.example.jwtsecurity.jwt.JWTUtil;
 import com.example.jwtsecurity.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Configuration
@@ -68,6 +73,23 @@ public class SecurityConfig {
                             new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
                             UsernamePasswordAuthenticationFilter.class);
 
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         return http.build();
     }
