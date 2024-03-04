85ef261cb2510cbc0e5b587372a6f5e34fc398c7
TaeWon <ytw9699@naver.com>
2024-03-04 23:36:25 +0900

    feat : OAuth2 클라이언트 JWT 9 : OAuth2UserService 응답 받기

--- OAuthJWT/src/main/java/com/example/oauthjwt/config/SecurityConfig.java
+++ OAuthJWT/src/main/java/com/example/oauthjwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.oauthjwt.config;
 
+import com.example.oauthjwt.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,9 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,7 +34,9 @@ public class SecurityConfig {
 
         //oauth2
         http
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)));
 
         //경로별 인가 작업
         http
