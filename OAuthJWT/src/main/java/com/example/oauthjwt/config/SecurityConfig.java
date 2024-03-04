af1d0f660d2826c2976dd08d331a67985935c653
TaeWon <ytw9699@naver.com>
2024-03-05 00:17:26 +0900

    feat : OAuth2 클라이언트 JWT 13 : 로그인 성공 JWT 발급

--- OAuthJWT/src/main/java/com/example/oauthjwt/config/SecurityConfig.java
+++ OAuthJWT/src/main/java/com/example/oauthjwt/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.example.oauthjwt.config;
 
+import com.example.oauthjwt.jwt.JWTUtil;
+import com.example.oauthjwt.oauth2.CustomSuccessHandler;
 import com.example.oauthjwt.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,7 +39,9 @@ public class SecurityConfig {
         http
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService)));
+                                .userService(customOAuth2UserService))
+                                .successHandler(customSuccessHandler)
+                );
 
         //경로별 인가 작업
         http
