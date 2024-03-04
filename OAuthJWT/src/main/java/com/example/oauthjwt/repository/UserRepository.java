6b26ae8feee0802db5c8bbb24302dcc39e5d5e30
TaeWon <ytw9699@naver.com>
2024-03-04 23:57:09 +0900

    feat : OAuth2 클라이언트 JWT 11 : 유저 정보 DB 저장

---
+++ OAuthJWT/src/main/java/com/example/oauthjwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.oauthjwt.repository;
+
+import com.example.oauthjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
--- OAuthJWT/src/main/java/com/example/oauthjwt/service/CustomOAuth2UserService.java
+++ OAuthJWT/src/main/java/com/example/oauthjwt/service/CustomOAuth2UserService.java
@@ -1,14 +1,21 @@
 package com.example.oauthjwt.service;
 
 import com.example.oauthjwt.dto.*;
+import com.example.oauthjwt.entity.UserEntity;
+import com.example.oauthjwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -31,12 +38,37 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         }
         //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
         String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
 
-        UserDTO userDTO = new UserDTO();
-                userDTO.setUsername(username);
-                userDTO.setName(oAuth2Response.getName());
-                userDTO.setRole("ROLE_USER");
+        if (existData == null) {//로그인 처음
+            UserEntity userEntity = new UserEntity();
+                       userEntity.setUsername(username);
+                       userEntity.setEmail(oAuth2Response.getEmail());
+                       userEntity.setName(oAuth2Response.getName());
+                       userEntity.setRole("ROLE_USER");
 
-        return new CustomOAuth2User(userDTO);
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+                    userDTO.setUsername(username);
+                    userDTO.setName(oAuth2Response.getName());
+                    userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }
+        else {
+
+            existData.setEmail(oAuth2Response.getEmail());//이메일이나 
+            existData.setName(oAuth2Response.getName());//이름이 변경된경우를 고려
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+                    userDTO.setUsername(existData.getUsername());
+                    userDTO.setName(oAuth2Response.getName());
+                    userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
     }
 }
